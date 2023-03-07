fcd896d92208fa81267561da05e1ff0736a17a81
Juan Pablo Favale <juanpablofavale@hotmail.com>
2023-03-06 21:55:02 -0300

    mejorando estilo a las tarjetas

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,14 @@ function renderizarTarjetas(arrTarjetas, contenedor) {
 
     for (tarjeta of arrTarjetas) {
         tarjetas += `
-            <div class="card p-0" style="width: 18em;">
+            <div class="card p-0" style="width: 16em;">
             <img src="${tarjeta.image}"
                 class="card-img-top" alt="cinema">
-            <div class="card-body">
-                <h5 class="card-title">${tarjeta.name}</h5>
-                <p class="card-text">${tarjeta.description}</p>
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h5 class="card-title">${tarjeta.name}</h5>
+                    <p class="card-text">${tarjeta.description}</p>
+                </div>
                 <div class="d-flex justify-content-between align-items-center">
                     <p class="precio mb-0">$${tarjeta.price}</p>
                     <a href="./details.html" class="btn btn-primary">Ver mas</a>
